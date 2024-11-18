37e436f05e0c070c060e1920da81b0bbf37c5429
Dinis Machado <137174511+DinisMachado87@users.noreply.github.com>
2024-11-18 01:12:05 +0100

    Add ft_memset

---
+++ ft_memset.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*memCharArr;
+
+	memCharArr = (unsigned char*)b;
+	i = 0;
+	while (i <= len)
+	{
+		memCharArr[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
