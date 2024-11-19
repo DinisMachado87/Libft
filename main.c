ee187ed6d45a348feb6531fddb5053e98631ce06
Dinis Machado <dimachad@student.42berlin.de>
2024-11-19 01:10:22 +0100

    Update main testing

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   testmain.c                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dimachad <dimachad@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 21:56:07 by dimachad          #+#    #+#             */
-/*   Updated: 2024/11/12 00:33:32 by dimachad         ###   ########.fr       */
+/*   Created: 2025/11/11 21:56:07 by dimachad          #+#    #+#             */
+/*   Updated: 2024/11/19 00:47:40 by dimachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stddef.h>
 #include <ctype.h>
-#include <libc.h>
 #include <string.h>
 
 int 	ft_isalpha(int c);
@@ -24,6 +23,9 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 struct	TestChar
 {
@@ -40,7 +42,6 @@ struct	TestString
 void	charTests(int (*function)(int), char *funcName, int (*original_function)(int), char *original_funcName)
 {
 	int		i_t_char;
-
 	struct	TestChar test_char[] =
 	{
 		{"alphabetical", 'a'},
@@ -93,7 +94,7 @@ void	strTests(size_t (*function)(const char*), char *funcName, size_t (*original
 	printf("\n\n");
 }
 
-void print_buffer(const char *buffer, size_t size)
+void	print_buffer(const char *buffer, size_t size)
 {
 	size_t	i;
 
@@ -101,22 +102,23 @@ void print_buffer(const char *buffer, size_t size)
 	while (size--)
 	{
 		printf("%02x ", (unsigned char)buffer[i]);
+		i++;
 	}
 	printf("\n");
 }
 
-void memsetTests(void *(*function)(void *, int, size_t), char *funcName, void *(*original_function)(void *, int, size_t), char *original_funcName)
+void	memsetTests(void *(*function)(void *, int, size_t), char *funcName, void *(*original_function)(void *, int, size_t), char *original_funcName)
 {
 	int i_t_char;
 	struct TestChar test_char[] =
-			{
-					{"alphabetical", 'a'},
-					{"numerical", '1'},
-					{"space", ' '},
-					{"null", '\n'},
-					{"non-ascii", 200},
-					{"invisible printable under 40", 34},
-			};
+		{
+			{"alphabetical", 'a'},
+			{"numerical", '1'},
+			{"space", ' '},
+			{"null", '\n'},
+			{"non-ascii", 200},
+			{"invisible printable under 40", 34},
+		};
 	i_t_char = 0;
 	while (i_t_char < 6)
 	{
@@ -130,13 +132,12 @@ void memsetTests(void *(*function)(void *, int, size_t), char *funcName, void *(
 		printf("%s: ", original_funcName);
 		print_buffer((const char *)buffer, 10);
 		printf("-----\n");
-
 		i_t_char++;
 	}
 	printf("\n");
 }
 
-void bzeroTests(void (*function)(void *, size_t), char *funcName, void (*original_function)(void *, size_t), char *original_funcName)
+void	bzeroTests(void (*function)(void *, size_t), char *funcName, void (*original_function)(void *, size_t), char *original_funcName)
 {
 	void	*ft_buffer[10];
 	void	*buffer[10];
@@ -150,7 +151,46 @@ void bzeroTests(void (*function)(void *, size_t), char *funcName, void (*origina
 	printf("\n");
 }
 
-int main(void)
+void	memcpyTests(void *(*ft_function)(void *, const void *, size_t),char *ft_name, void *(*function)(void *, const void *, size_t), char *name)
+{
+	char	*ft_buffer = "1234564899";
+	char	*buffer = "1234564899";
+	void	*ft_bufferCpy[10];
+	void	*bufferCpy[10];
+
+	ft_function(ft_bufferCpy, ft_buffer, 10);
+	function(bufferCpy, buffer, 10);
+	printf("\n-----\n%s: ", ft_name);
+	print_buffer((const char *)ft_buffer, 10);
+	printf("%s: ", name);
+	print_buffer((const char *)buffer, 10);
+	printf("\n-----\n\n");
+}
+
+void	strcpyTests()
+{
+	struct TestString test_string[] =
+	{	
+		{"alphanumeric", "a8a7d7fg0g9g51n3m4"},
+		{"alphabetical", "asdfghkjlz"},
+		{"numerical", "1234567890"},
+		{"empty", ""},
+		{"space", " "},
+		{"null", "\n"},
+	};
+	int	i;
+
+	i = 1;
+	printf("-----\nstrlcpy\n-----\n");
+	while(i < 6)
+		{
+			ft_strlcpy(test_string[0].str, test_string[i].str, strlen(test_string[i].str));
+			i++;
+		}
+	printf("\n-----\n\n");
+}
+
+int	main(void)
 {
 	charTests(ft_isdigit, "ft_isdigit", isdigit, "isdigit");
 	charTests(ft_isalpha, "ft_isalpha", isalpha, "isalpha");
@@ -163,6 +203,9 @@ int main(void)
 	memsetTests(ft_memset, "ft_memset", memset, "memset");
 	printf("-----\nbzero tests\n-----\n-----\n");
 	bzeroTests(ft_bzero, "ft_bzero", bzero, "bzero");
+	memcpyTests(ft_memcpy, "ft_memcpy", memcpy, "memcpy");
+	memcpyTests(ft_memmove, "ft_memmove", memmove, "memmove");
+	strcpyTests();	
 	return (0);
 }
 
