e6d60735ad7188b8a0d861d9ca46a0d5460ecf7a
Dinis Machado <137174511+DinisMachado87@users.noreply.github.com>
2024-12-10 11:36:49 +0000

    Debug count_words and extract_words helpers to ft_split

--- ft_split.c
+++ ft_split.c
@@ -12,72 +12,95 @@
 
 #include "libft.h"
 
-static char		*extract_word(char *str, char splitter)
+static size_t	count_words(const char *str, char splitter)
+{
+	size_t	num_words;
+	char	prev_char;
+
+	num_words = 0;
+	prev_char = splitter;
+	while (*str)
+	{
+		if (prev_char == splitter && *str != prev_char)
+			num_words++;
+		prev_char = *str;
+		str++;
+	}
+	return (num_words);
+}
+static char		*extract_word(const char *str, char splitter)
 {
 	char	*word;
 	size_t	word_len;
 	size_t	i_word;
 
-	word_len  = 0;
+	word_len = 0;
 	while (str[word_len] && str[word_len] != splitter)
 		word_len++;
-	word = (char *)malloc(word_len * sizeof(char));
+	word = (char *)malloc((word_len + 1) * sizeof(char));
 	if (!word)
 		return (NULL);
 	i_word = 0;
-	while (i_word <= word_len)
+	while (i_word < word_len)
 	{
 		word[i_word] = str[i_word];
 		i_word++;
 	}
+	word[i_word] = '\0';
 	return (word);
 }
 
-
-static size_t	count_splitters(char *s, char splitter)
+/*
+static char *store_word_in_arr(const char *str, char splitter, char **word_arr, size_t i_arr)
 {
-	size_t	i_str;
-	size_t	num_words;
-
-	i_str = 0;
-	num_words = 1;
-	while (s[i_str])
+	if (str)
 	{
-		if (s[i_str] == splitter)
-			num_words++;
-		i_str++;
+		word_arr[i_arr] = extract_word(str, splitter);
+		if (!word_arr[i_arr])
+		{	
+			while (i_arr >= 0)
+			{
+				free(word_arr[i_arr]);
+				i_arr--;
+			}
+			free(word_arr);
+		}
+		return (NULL);
 	}
-	return (num_words);
+	return (word_arr[i_arr]);
 }
 
-char	**ft_split(char const *s, char splitter)
+char	**ft_split(const char *str, char splitter)
 {
 	char	**word_arr;
 	size_t	arr_size;
 	size_t	i_arr;
-	char	*remaining_str;
 
-	remaining_str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));	
-	ft_strlcpy(remaining_str, s, ft_strlen(s));
-	arr_size = 1 + count_splitters(remaining_str, splitter);
+	arr_size = 1 + count_words(str, splitter);
 	word_arr = (char **)malloc(arr_size * sizeof(char *));
 	if (!word_arr)
 		return (NULL);
 	i_arr = 0;
 	while (i_arr < arr_size - 1)
 	{
-		extract_left(&remaining_str, word_arr, i_arr, splitter);
+		while (*str && *str == splitter)
+			str++;
+		word_arr[i_arr] = store_word_in_arr(str, splitter, word_arr, i_arr);
+		while (*str != splitter)
+			str++;
 		i_arr++;
 	}
-	word_arr[arr_size - 1] = NULL;
+	word_arr[i_arr] = NULL;
 	return(word_arr);
 }
-/*
+*/
+
 #include <stdio.h>
 
 int		main()
 {
-	ft_split("sddjlksdflsdkdjlsd", 'k');
+	printf("count_words: %zu\n", count_words("sddjlkksdflsdkdjlsd", 'k'));
+	printf("extract_word: %s\n", extract_word("sddjlkksdflsdkdjlsd", 'k'));
 	return (0);
 }
-*/
+
